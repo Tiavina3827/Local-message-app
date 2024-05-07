f35c19e38b01e0d8fe4e87d1c994eda542580a1b
Tiavina <hei.tiavina.4@gmail.com>
2024-05-07 07:52:31 +0300

    feat: front-end basics done

--- chatroom/server.js
+++ chatroom/server.js
@@ -3,17 +3,20 @@ const path =require("path");
 
 const app = express();
 const server = require("http").createServer(app);
-const io= require("socket.io")(server);
+
+// const io= require("socket.io")(server);
+
 app.use(express.static(path.join(__dirname+"/public")));
 server.listen(5000);
-io.on("connection", function(socket){
-    socket.on("newuser",function(username){
-        socket.broadcast.emit("update",username+"Joined the conversation");
-    });
-    socket.on("exituser",function(username){
-        socket.broadcast.emit("update",username+"left the conversation");
-    });
-    socket.on("message",function(username){
-        socket.broadcast.emit("chat",message);
-    });
-})
+
+// io.on("connection", function(socket){
+//     socket.on("newuser",function(username){
+//         socket.broadcast.emit("update",username+"Joined the conversation");
+//     });
+//     socket.on("exituser",function(username){
+//         socket.broadcast.emit("update",username+"left the conversation");
+//     });
+//     socket.on("message",function(username){
+//         socket.broadcast.emit("chat",message);
+//     });
+// })
